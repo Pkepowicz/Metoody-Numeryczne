3e25860ce8127c609949bcc0f123a206c027bb35
Pkepowicz <paw.kepowicz@gmail.com>
2022-11-15 16:36:22 +0100

    another update

--- Lab4/problem4_4.m
+++ Lab4/problem4_4.m
@@ -5,42 +5,31 @@ clear all; close all;
 clear all; close all;
 A = [ 1 2; ...
       3 5 ];
+%A = A+0.01*randn(size(A)),  %Szum
 b = [  5; ...
        11  ];
 disp(det(A));
 x1 = inv(A)*b,          % x=A^(-1)*b
 x2 = A\b,               % optymalne rozwiazywanie rown. Ax=b
-%x3 = pinv(A)*b,        % x = inv( A'*A ) * A' * b , sprawdzisz?
+x3 = pinv(A)*b,         % x = inv( A'*A ) * A' * b , sprawdzisz?
 bhat = A*x1,            % sprawdzenie
 err = max(abs(x1-x2)),  % blad
-%Oryginalnie wszystko ok.
+
 pause
 clc
-
-%Przy zmianie wyznacznika z -2 na 1 nadal wszystko ok
-%Przy zmianie wyznacznika z -2 na 0.5 (A(1,2) == 7/6) err wynosi 1,7764e-15
-%Przy zmianie wyznacnzika z -2 na -0.02 bład (A(1,2) == 1.34) wynosi 5.6843e-14
-A = A+0.01*randn(size(A)),
-x3 = inv(A)*b,
-x4 = A\b,
-bhat = A*x2,
-err = max(abs(x3-x4)),
-pause
-clc
-
-%W tym przypadku b różni się nieznacznie od oryginalnego a err wynosi 4.4409e-16
-%Wektor x również się różni
-C = [1 52 43; ...
-    101 51 633;
-    79 1123 9],
-d = [1; ...
-    2;
-    3],
+%C = [4 -2 44;
+%     8 -6 1;
+%     4 3 -2],
+C = [1 0.75 0.9;
+     0.9 0.5 1;
+     2 1.5 0.75],
+d = [-1; ...
+    -2;
+    1],
 disp(det(C));
 x5 = inv(C)*d,
 x6 = C\d,
 x7 = pinv(C)*d,
-dhat = C*x5,
+dhat = C*x6,
 err = max(abs(x5-x6)),
-%W tym przypadku dhat nie różni się od d(w użytej precyzji) a err wynosi 3.4694e-18
-%Tutaj właściwie wystarczy wszystko przepisywać jak w oryginalnym przykladzie tylko dobranie macierzy C jest irytujące XD
+
