04adc2a2eff54e13e7aa8659dfaaa850f2a765dd
Pkepowicz <paw.kepowicz@gmail.com>
2022-11-22 17:04:49 +0100

    lab5 upd

--- Lab5/problem5_16.m
+++ Lab5/problem5_16.m
@@ -9,7 +9,7 @@ Missing_X = X(2:2:120,:);                                           %Macierz zaw
 xvar = min(x) : (max(x)-min(x))/200 : max(x); % zmiennosc x
 yvar = min(y) : (max(y)-min(y))/200 : max(y); % zmiennosc y
 [Xi,Yi] = meshgrid( xvar, yvar );                                   % siatka interpolacji xi, yi
-out = griddata( x, y, z, Xi,Yi, 'nearest' );                          % interp: nearest, linear, spline (v4), cubic
+out = griddata( x, y, z, Xi,Yi, 'natural' );                          % interp: nearest, linear, natural, spline (v4), cubic
 figure; surf( out, LineStyle=":", LineWidth=0.1 ); pause  % rysunek
 half_x_var = min(half_x) : (max(half_x)-min(half_x))/200 : max(half_x); % Wychodzi na to samo co xvar
 half_y_var = min(half_y) : (max(half_y)-min(half_y))/200 : max(half_y); % Wychodzi na to samo co yvar
@@ -17,10 +17,11 @@ half_y_var = min(half_y) : (max(half_y)-min(half_y))/200 : max(half_y); % Wychod
 figure; grid; plot3( half_x, half_y, half_z, 'b.' ); pause
 
 %Interpolacja ======================================================
-half_out = griddata(half_x, half_y, half_z, half_Xi, half_Yi, 'nearest');
+half_out = griddata(half_x, half_y, half_z, half_Xi, half_Yi, 'natural');
 figure; surf( half_out, LineStyle=":", LineWidth=0.1 );
 %Sumowanie wartości błędów bezwzględnych.=================================
 l_err_abs_sum = 0;
+pa=0;
 [M,N] = size(Missing_X);
 rounded_xvar = round(xvar,4); % Zaokrąglam, ponieważ dla funkcji find 5.14999998 != 5.15
 rounded_yvar = round(yvar,4);
@@ -28,13 +29,16 @@ for i = 1:M
    x_var_index = find(rounded_xvar == Missing_X(i,1));
    y_var_index = find(rounded_yvar == Missing_X(i,2));
    l_err_abs_sum = l_err_abs_sum + abs(half_out(y_var_index,x_var_index)-out(y_var_index,x_var_index));
+   pa=pa+1;
 end
-l_err_abs_sum,
+l_err_abs_sum/pa,
 %Sumowanie błędó =================================
 l_err_sum = 0;
+pe=0;
 for i = 1:M
    x_var_index = find(rounded_xvar == Missing_X(i,1));
    y_var_index = find(rounded_yvar == Missing_X(i,2));
    l_err_sum = l_err_sum + (half_out(y_var_index,x_var_index)-out(y_var_index,x_var_index));
+   pe=pe+1;
 end
-l_err_sum,
+l_err_sum/pe,
---
+++ Lab5/problem5_2.m
@@ -0,0 +1,25 @@
+% interp_circle.m
+% x(i) = ax0 +ax1*i^1 + ax2*i^2 + ... + axN*i^N
+% y(i) = ay0 +ay1*i^1 + ay2*i^2 + ... + ayN*i^N
+
+clear all; close all;
+N = 4; % stopien wielomianow
+i = (0 : N)'; % zmienna "i" wielomianu w wezlach ("rzadka")
+xi = cos( 2*pi/N * i ); % wartosci funkcji x=kosinus w wezlach
+yi = sin( 2*pi/N * i ); % wartosci funkcji y=sinus w wezlach
+[ i, xi, yi ], % sprawdzenie wartosci
+
+X = vander(i), % wygenerowanie i pokazanie macierzy Vandermonde’a
+ax = inv(X) * xi; % obliczenie wielu wsp. wielomianu dla zmiennej x
+ay = inv(X) * yi; % obliczenie wielu wsp. wielomianu dla zmiennej y
+
+id = 0 : 0.01 : N; % zmienna "i" dokladna
+xd = cos( 2*pi/N * id ); % dokladne wartosci x
+yd = sin( 2*pi/N * id ); % dokladne wartosci y
+figure; plot( xi,yi,'ko', xd,yd,'r--', polyval(ax,id), polyval(ay,id),'b.-');
+xlabel('x'); ylabel('y'); title('y=f(x)'); axis equal; grid;
+
+% Dla n =4 - duzy blad
+% n = 5 - duzy
+%n = 6 - nadal widac czerwony okrag
+%n = 7 - ok
